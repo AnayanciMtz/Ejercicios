a356f4e459300ece559f55034edc93c2191f26bc
AnayanciMtz <anayanci1009@gmail.com>
2025-03-28 15:55:33 -0600

    Add files via upload

--- Ejercicio2.php
+++ Ejercicio2.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Información de PHP</title>
+    <title>Hola Mundo</title>
 </head>
 <body>
-    <h1>Detalles de configuración de PHP</h1>
-    <?php 
-        phpinfo(); 
-    ?>
+    <h1>El famoso script Hello World</h1>
+    <p>
+        <?php
+            echo "Hola Mundo!";
+        ?>
+    </p>
 </body>
 </html>
